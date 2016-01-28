cd26c477d7d98081dcd65fc0ad2bec707e9aff25
Vitaliy Timchenko (lenovo) <vitaliy.timchenko@gmail.com>
2016-01-28 13:26:03 +0200

    Make radiobuttons work

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@
 var currentStep = 0,
     nextStep,
     maxStep,
+    stepStack = [],
     currentFieldset,
     nextFieldset,
     form = $('#mform'),
@@ -10,7 +11,9 @@ var currentStep = 0,
     btnPrev = $('#mform input.prev'),
     btnNext = $('#mform input.next'),
     fieldsets = $('#mform fieldset'),
-    progressbar = $('#progressbar li');
+    progressbar = $('#progressbar li'),
+    radioProd = $('#mform input[name="prod"]'),
+    radioProdType = $('#mform input[name="prodType"]');
 
 function showStep(nextStep) {
 
@@ -29,7 +32,7 @@ function showStep(nextStep) {
     nextFieldset.show();
 
     showButtons(nextStep);
-    
+
     if (nextStep > currentStep) {
         progressbar.eq(currentStep).removeClass('active');
         progressbar.eq(currentStep).addClass('pass');
@@ -38,6 +41,20 @@ function showStep(nextStep) {
         progressbar.eq(currentStep).removeClass('active');
         progressbar.eq(nextStep).removeClass('pass');
         progressbar.eq(nextStep).addClass('active');
+    }
+    
+    if (nextStep == 1) {
+        progressbar.eq(1).show();
+    }
+    
+    if (nextStep == 0) {
+        progressbar.eq(1).hide();
+    }
+
+    // Do not remeber last step if we go back 
+    // to prevent loop between steps
+    if (nextStep > currentStep) {
+        stepStack.push(currentStep);
     }
     
     currentStep = nextStep;
@@ -56,18 +73,18 @@ function showButtons(step) {
     if (step > 0) {
         btnPrev.show();
     }
-    
+
     if (step < maxStep && btnSubmit.is(':visible')) {
         btnSubmit.hide();
         btnNext.show();
     }
 }
 
-function showProgress (step) {
+function showProgress(step) {
     progressbar.eq(step).addClass('active');
 }
 
-function hideProgress (step) {
+function hideProgress(step) {
     progressbar.eq(step).removeClass('active');
 }
 
@@ -82,13 +99,34 @@ $(document).ready(function () {
     // Hide buttons
     btnSubmit.hide();
     btnPrev.hide();
+    
+    // Hide second step on progressbar
+    progressbar.eq(1).hide();
 
     btnNext.click(function () {
         showStep(currentStep + 1);
     });
 
     btnPrev.click(function () {
-        showStep(currentStep - 1);
+        showStep(stepStack.pop());
+    });
+
+    radioProd.click(function () {
+        // Deselect productType checked if there
+        radioProdType.filter(':checked').prop('checked', false);
+
+        showStep(currentStep + 1);
+    });
+
+    radioProdType.click(function () {
+        if (currentStep == 0) {
+            // Deselect product checked if there
+            radioProd.filter(':checked').prop('checked', false);
+            progressbar.eq(1).hide();
+            showStep(currentStep + 2);
+        } else {
+            showStep(currentStep + 1);
+        }
     });
 
     maxStep = fieldsets.length - 1;
